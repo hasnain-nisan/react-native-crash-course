6e46fd237f8b318e7e666a500e6ee241b6fdc87c
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2024-04-17 16:36:54 +0600

    started working in auth

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,23 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants'
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" className='text-blue-400 font-pbold'>Go to home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView 
+        contentContainerStyle={{
+          height: '100%'
+        }}
+      >
+        <View className="w-full h-full justify-center items-center px-4">
+          <Image
+            source={images.logo}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
